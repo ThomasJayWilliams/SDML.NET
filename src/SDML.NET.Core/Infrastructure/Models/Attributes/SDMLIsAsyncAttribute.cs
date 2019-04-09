f6ca3d4d332efae455c12088aafaa8052e8c2d7f
Paul Moon <bakamono.moon@gmail.com>
2019-04-09 13:53:15 -0400

    Update SDMLIsAsyncAttribute.cs

--- src/SDML.NET.Core/Infrastructure/Models/Attributes/SDMLIsAsyncAttribute.cs
+++ src/SDML.NET.Core/Infrastructure/Models/Attributes/SDMLIsAsyncAttribute.cs
@@ -1,6 +1,6 @@
 ﻿namespace SDML.NET.Core.Infrastructure.Models.Attributes
 {
-		//Class to check IsAysnc Object value
+		//Class to mark IsAysnc Object value
 	public class SDMLIsAsyncAttribute : SDMLBaseAttribute
 	{
 		public override string ObjectName { get; } = "IsAsync";
