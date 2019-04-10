c44e65b058083749d63962ebd4e4dd53de0e9d88
Paul Moon <bakamono.moon@gmail.com>
2019-04-09 23:09:08 -0400

    Add files via upload

---
+++ src/SDML.NET.Core/Infrastructure/Models/Comments/SdmlComment.cs
@@ -0,0 +1,10 @@
+﻿using SDML.NET.Core.Infrastructure.Abstractions;
+
+namespace SDML.NET.Core.Infrastructure.Models
+{
+    public class SdmlComment : ISDMLComment
+    {
+        public string ObjectName { get; } = "//";
+        public string Value { get; set; }
+    }
+}
